c6703dd2c5e64bb64dd815a54849fc3768d5257f
sekkarin <sekkri1234@gmail.com>
2022-02-17 18:30:57 +0700

    success page job

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,18 @@ ScrollReveal({ duration: 600 })
 ScrollReveal().reveal('.nav-bar', { delay: 200 })
 ScrollReveal().reveal('.delay-1', { delay: 600 })
 ScrollReveal().reveal('.delay-2', { delay: 1200 })
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: -25.344, lng: 131.036 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+  }
